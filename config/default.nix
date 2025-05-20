565b7f04edbe92f29cd8f8bde0a49b30dc0f0c6d
johncoopertr <102762460+johncoopertr@users.noreply.github.com>
2025-05-20 16:08:29 -0700

    Update default.nix

--- config/default.nix
+++ config/default.nix
@@ -15,4 +15,18 @@
     mapleader = " ";
     maplocalleader = " ";
   };
+  clipboard = "unnamedplus"; 
+  extraLuaConfig = ''
+    vim.g.clipboard = {
+      name = 'OSC 52',
+      copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+      },
+      paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+      },
+    }
+  '';
 }
